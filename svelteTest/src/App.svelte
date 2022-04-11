2334dd97c4448f5d9482dbf297f2b56f18deffdf
eygis <86146074+eygis@users.noreply.github.com>
2022-04-11 22:38:34 +0900

    wording change

--- svelteTest/src/App.svelte
+++ svelteTest/src/App.svelte
@@ -61,8 +61,8 @@
 
   //progress
   let progress = writable(0);
-  let progresser = (input) => {
-    progress.set(input);
+  let progresser = (percent) => {
+    progress.set(percent);
   };
 </script>
 
